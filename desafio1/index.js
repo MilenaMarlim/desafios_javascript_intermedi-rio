e907081b0ee094f70ad5620ec06f389573278702
Milena Marlim <milenamarlim.dev@gmail.com>
2022-03-10 17:38:51 -0300

    Commit com a criação incial do desafio 1 - construção do html, css e inicio do raciocínio do JS

---
+++ desafio1/index.js
@@ -0,0 +1,21 @@
+
+let setaVoltar = document.getElementsByClassName('seta-de-voltar')
+let setaAvancar = document.getElementsByClassName('seta-de-avancar')
+
+let carrosselPrincipal = document.getElementsByClassName('principal')
+
+let carrosselImagens = document.getElementsByClassName('imagens_gerais')
+
+function passarImagem(){
+    if carrosselPrincipal === 1{
+
+    }else{
+        return
+    }
+
+}
+
+passarImagem.addEventListener ('change', function(){
+    for(i = 1, i<5, i++){
+        
+    } 
